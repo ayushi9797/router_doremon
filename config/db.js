3cf31d2659ba11c8965776a022f9c7f7dcfb9fe7
ayushi9797 <soniayushi345@gmail.com>
2023-01-16 17:35:51 +0530

    final

--- config/db.js
+++ config/db.js
@@ -2,4 +2,4 @@ const mongoose=require("mongoose");
 mongoose.set('strictQuery', false);
 require("dotenv").config()
 const connection = mongoose.connect(process.env.mongoURL);
-module.exports={connection,}
+module.exports={connection}
